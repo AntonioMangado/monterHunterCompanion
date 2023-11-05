e5b14e6cfaf9635cf62cb8b4ce367181ced68511
AntonioMangado <antonio.mangado@gmail.com>
2023-11-05 21:32:24 +0400

    ADD/ weapon list now generates

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 // Global variables
 const landing = document.getElementById("landing");
 const startingBtn = document.querySelector(".starter");
-const monsterSect = document.getElementById("monster-cards");
-
+const monsterSect = document.querySelector(".cards");
+const weaponSect = document.getElementById("weapons");
 
 // Events
 startingBtn.addEventListener("click", async function() {
@@ -25,8 +25,8 @@ startingBtn.addEventListener("click", async function() {
     let list = "<h3>Choose a monster to hunt:</h3>"
     for (let i = 0; i < largeMonsters.length; i++) {
         list += `<button><article class="card">
-                    <img class="monster-img" src="./assets/images/monster icons/${largeMonsters[i].name}.webp" alt="${largeMonsters[i].name}">
-                    <p class="monster-name">${largeMonsters[i].name}</p>
+                    <img class="item-img" src="./assets/images/monster icons/${largeMonsters[i].name}.webp" alt="${largeMonsters[i].name}">
+                    <p class="item-name">${largeMonsters[i].name}</p>
                 </article></button>`
     }
 
@@ -34,11 +34,10 @@ startingBtn.addEventListener("click", async function() {
     monsterSect.innerHTML = list;
 
     // Store selection in LocalStorage
-    const monsterBtns = document.querySelectorAll("#monster-cards button")
     const monsterCards = document.querySelectorAll(".card")
 
     for (let i = 0; i < monsterCards.length; i++) {
-        monsterCards[i].addEventListener("click", function () {
+        monsterCards[i].addEventListener("click", async function () {
             
             console.log(`You chose the monster: ${largeMonsters[i].name}`)
 
@@ -52,10 +51,41 @@ startingBtn.addEventListener("click", async function() {
 
             //Hide the monster section
             setTimeout(makeMonstersDisappear, 2000);
-            ;
+
+            // Fetch weapon data
+            let res = await fetch("https://mhw-db.com/weapons")
+            let data = await res.json()
+
+            // Find weapons with rarity6+
+            let highRankWeapon = data.filter(weapon => weapon.rarity >= 6)
+
+            // Filter by elemental weapons
+            let elementalWeapons = highRankWeapon.filter(obj => obj.elements.length > 0)
+
+            // Filter by monster weakness
+            let weaknessWeapon = elementalWeapons.filter(obj => obj.elements[0].type == monsterWeakness.element)
+            
+
+            // Draw them on the DOM
+            let weaponList = "<h3>Choose a weapon for your hunt:</h3>";
+            let weaponsArr = [];
+            for (let i = 0; i < weaknessWeapon.length; i++) {
+                if (!weaponsArr.includes(weaknessWeapon[i].type)) {
+
+                    weaponList += `<button><article class="card">
+                                        <img class="item-img" src="./assets/images/weapon icons/${weaknessWeapon[i].type}.webp" alt="${weaknessWeapon[i].type}">
+                                        <p class="item-name">${weaknessWeapon[i].type.replace("-", " ")}</p>
+                                    </article></button>`
+                    weaponsArr.push(weaknessWeapon[i].type)
+                }
+            }
+
+            // Weapon section shows up
+            weaponSect.style.display = null;
+            weaponSect.style.display = "flex";
+            weaponSect.innerHTML = weaponList; 
         }) 
     }
-    
 })
 
 // Functions
